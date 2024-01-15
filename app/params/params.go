60c32aefda8fb7b145986ce15aa33724e095105b
Tuan Tran <tuantran@notional.ventures>
2024-01-15 11:01:02 +0700

    add MinSelfDelegation and DefaultPowerReduction to app params for reuse

--- app/params/params.go
+++ app/params/params.go
@@ -1,9 +1,14 @@
 package params
 
+import sdk "github.com/cosmos/cosmos-sdk/types"
+
 // Simulation parameter constant
 const (
 	StakePerAccount           = "stake_per_account"
 	InitiallyBondedValidators = "initially_bonded_validators"
 	BondDenom                 = "acudos"
 	AdminTokenDenom           = "cudosAdmin"
+	MinSelfDelegation         = "2000000000000000000000000"
 )
+
+var DefaultPowerReduction = sdk.NewIntFromUint64(1000000000000000000)
